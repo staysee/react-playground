d44057d8a4aa3233f4c478e2d75c6c33a6d52140
Sugiono <ssugiono@vacco.com>
2019-09-10 16:45:12 -0700

    completed Bomb and RouletteGun drills

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import HelloWorld from './state-drills/HelloWorld'
+import Bomb from './state-drills/Bomb'
+import RouletteGun from './state-drills/RouletteGun'
+
 import './index.css';
 
-ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+ReactDOM.render(<RouletteGun />, document.getElementById('root'));
---
+++ src/state-drills/Bomb.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+class Bomb extends React.Component {
+    state = {
+        count: 0
+    }
+
+    componentDidMount() {
+        this.interval = setInterval( () => {
+            this.setState({
+                count: this.state.count + 1
+            })
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
+    }
+
+    bombCounter() {
+        const { count } = this.state
+        if (count >= 8) {
+            clearInterval(this.interval)
+            return 'BOOM!!!!'
+        }
+        else if (count % 2 === 0){
+            return 'tick'
+        }
+        else {
+            return 'tock'
+        }
+    }
+    render() {
+        return (
+            <div className="BombCounter">
+                <p>{this.bombCounter()}</p>
+            </div>
+        )
+
+    }
+}
+
+export default Bomb
