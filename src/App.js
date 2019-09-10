dc788fc078d554ff87a94e963987b6be97afc0bd
Sugiono <ssugiono@vacco.com>
2019-09-10 15:57:17 -0700

    created counter and date components to learn setstate and event handlers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Split from './composition/Split';
 import './App.css';
 import Tooltip from './composition/Tooltip';
+import TheDate from './state/TheDate'
+import Counter from './state/Counter'
 
 //make 2 tooltips here and another inside the App directly
 const firstTooltip = (
@@ -16,22 +18,37 @@ const secondTooltip = (
   </Tooltip>
 )
 
-function App() {
-  return (
-    <main className='App'>
-      <Split className='left' flexBasis='2'>
-      This is the content for the left Split. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
-        {/*make another tooltip directly inside the App */}
-        <Tooltip message = 'one more tooltip message'>
-          Necessitatibus?
-        </Tooltip>
-      </Split>
+// function App() {
+//   return (
+//     <main className='App'>
+//       <Split className='left' flexBasis='2'>
+//       This is the content for the left Split. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
+//         {/*make another tooltip directly inside the App */}
+//         <Tooltip message = 'one more tooltip message'>
+//           Necessitatibus?
+//         </Tooltip>
+//       </Split>
 
-      <Split className='right'>
-      This is the content for the right Split. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
-      </Split>
-    </main>
-  );
+//       <Split className='right'>
+//       This is the content for the right Split. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
+//       </Split>
+//     </main>
+//   );
+// }
+
+class App extends React.Component {
+  static defaultProp = {
+    step: 1
+  }
+  render() {
+    return (
+      <main className='App'>
+        <TheDate />
+        <Counter count={123} step={2} />
+      </main>
+    )
+
+  }
 }
 
 export default App;
--- src/state/TheDate.js
+++ src/state/TheDate.js
@@ -3,12 +3,28 @@ import React from 'react';
 class TheDate extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = { datetime: new Date() }
+        console.log('constructor')
+    }
+
+    componentDidMount() {
+        console.log('componenentDidMount')
+        this.interval = setInterval(() => {
+            console.log('setInterval')
+            this.setState({
+                datetime: new Date()
+            })
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
     }
 
     render() {
+        console.log('render')
         return(
-            <div />
+            <div>{this.state.datetime.toLocaleString()}</div>
         )
     }
 }
