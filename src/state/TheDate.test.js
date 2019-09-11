d5fe47ff01dd90e0659ecd6bc570be524989de62
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-09-10 18:48:43 -0700

    old changes

---
+++ src/state/TheDate.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+import TheDate from './TheDate'
+
+
+describe('TheDate Component', () => {
+    it ('renders without crashing')
+})
