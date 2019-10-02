51e3b2ef274fab547dbac64f9683f1f39b0cf74c
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-10-02 15:08:58 -0700

    copied files for paltrows power toes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
+
 import App from './App';
 import HelloWorld from './state-drills/HelloWorld'
 import Bomb from './state-drills/Bomb'
@@ -7,4 +9,8 @@ import RouletteGun from './state-drills/RouletteGun'
 
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App /> 
+    </BrowserRouter>,
+    document.getElementById('root'));
