f05d8fe3da4824dcb7fefcf0b5882dc9ea21bfdc
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-10-22 16:22:45 -0700

    added error boundary around the curencies

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 
 // import App from './App';
-import App from './paltrows-power-toes/App'
+// import App from './paltrows-power-toes/App'
 import HelloWorld from './state-drills/HelloWorld'
 import Bomb from './state-drills/Bomb'
 import RouletteGun from './state-drills/RouletteGun'
 import AppLang from '../src/lang-context/AppLang'
 import RegistrationForm from '../src/registrationForm/registrationForm'
-
+import App from './playingwithdanger/App'
 import './index.css';
 
 ReactDOM.render(
-        <RegistrationForm />,
+        <App />,
     document.getElementById('root'));
